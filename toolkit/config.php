f0548fe8150c276f73ffd67f6e12841a872ba9d6
wi1dcard <wi1dcard@qq.com>
2018-10-15 17:28:53 +0800

    修复空项目报错问题

--- toolkit/config.php
+++ toolkit/config.php
@@ -48,7 +48,7 @@ return [
         '周报工具箱' => [                     ## 项目名称，随意起名
             'path' => dirname(__DIR__),     ## [必填] Git 仓库本地目录；请填绝对路径。
             'url' => 'get_remote_url',      ## [可选] Git 仓库在线地址；若不填则周报提交记录将是纯文本，不带超链接；使用 `get_remote_url` 将会根据 remote origin 自动检测，实际上这是个 callable function，你可以在 helpers.php 找到它。
-            'pathspec' => '',               ## [可选] 可用于排除 / 包含指定目录，参见 https://kgrz.io/git-intro-to-pathspec.html。
+            'pathspec' => null,             ## [可选] 可用于排除 / 包含指定目录，参见 https://kgrz.io/git-intro-to-pathspec.html。
         ],
         // ... 你可以添加更多项目
     ]
